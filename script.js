bbf5379275ba049e9dd2de2ea08eb50615be4c04
MMZaini <mahdizainipro@gmail.com>
2021-09-08 16:40:59 +0100

    custom overdraft

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let balance = 0
-let overDraft = -5000
+let overDraft = -1000
 
+document.getElementById("odLimit").innerHTML = "<input id='changeOd' value='-1000' type='text' class='form-control' placeholder='-1000'>"
 
 let depositBox = document.getElementById("deposit").value
 let withdrawBox = document.getElementById("withdraw").value
@@ -15,7 +16,8 @@ function deposit() {
 
 function withdraw() {
   withdrawBox = document.getElementById("withdraw").value
-  if (balance - withdrawBox > overDraft) {
+  overDraft = document.getElementById("changeOd").value
+  if (balance - withdrawBox > overDraft - 1) {
     balance = balance - parseFloat(withdrawBox)
     balanceAdjust = "Balance: " + balance.toFixed(1)
     document.getElementById("balance").innerText = balanceAdjust
